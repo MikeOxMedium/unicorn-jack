00c3a6d1edec8406ba94b74683981df6422aa2f7
MikeOxMedium <111542535+MikeOxMedium@users.noreply.github.com>
2023-02-28 12:59:25 +1300

    Add files via upload

---
+++ 01_sandpit.py
@@ -0,0 +1,8 @@
+# Ask users for a number
+get_number = int(input("Choose a number? "))
+
+# Multiply the numbers_five = get_number * 5
+times_five = get_number * 5
+
+# Output the result
+print(f"{get_number} x 5 = {times_five}")
